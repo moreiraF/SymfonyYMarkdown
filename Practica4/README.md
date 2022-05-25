0efd9ac04ec77ad52fd9db637e1c995cd1b7203e
Fran Moreira Romero <91666471+moreiraF@users.noreply.github.com>
2022-05-25 11:39:52 +0200

    Add files via upload

---
+++ Practica4/README.md
@@ -0,0 +1,31 @@
+# Symfony Practica4 y Markdown
+
+## Descripción del proyecto
+*Practica de Symfony para poder aprender las diferentes funciones que tiene Symfony*
+
+### Como usar el proyecto y que entornos de desarollo se necesitan
+*Para usar este proyecto se necesita symfony para ser lanzado, tambien crear una base de datos con SQLite y un codigo php con plantillas. Despues lanzaremos el proyecto y así podremos navegar por las diferentes secciones de la pagina web que hemos creado.*
+
+#### Requisitos del entorno de desarrollo
+*Se requiere tener instalado Symfony y el composer.*
+
+# Guia de instalación y funcionamiento
+*Se necesita Symfony y doctrine. Tambien SQLite o un programa que sea capaz de gestionar una bbdd y un Visual Studio Code para editar y trabajar en nuestro código.*
+
+## Tecnologias utilizadas
+
+|Programas utilizados|Nivel del programa|
+| ----------- | ------------|
+| [PHP](https://www.php.net/)|media|
+| [Symfony](https://symfony.com/)|media|
+| [SQLite](https://www.sqlite.org/)|facil|
+
+### Autor
+**Fran Moreira**
+
+#### Licencia
+*MIT license*
+
+# Como contribuir al proyecto
+
+*Podeis contribuir a modificar y mejorar el proyecto de cualquier forma tratandose de un proyecto de codigo abierto*
---
+++ Practica4/src/Controller/ArticuloController.php
@@ -0,0 +1,127 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+use App\Entity\Articulo; 
+use App\form\ArticuloType;
+
+
+class ArticuloController extends AbstractController
+{
+    /**
+     * @Route("/" , name="/")
+     */
+    public function paginaInicio(ManagerRegistry $doctrine): Response {
+        $repository = $doctrine->getRepository(Articulo::class);
+        $articulos = $repository->findAll();
+
+
+        return $this->render('home.html.twig', [
+            "articulos" => $articulos
+        ]);
+    }
+
+
+    /**
+     * @Route("/ver/{numero}")
+     */
+    public function inforArticulo(ManagerRegistry $doctrine, $numero) {
+
+        $repository = $doctrine->getRepository(Articulo::class);
+        $articulo = $repository->findOneBy([
+            "id" => $numero
+        ]);
+
+        return $this->render('ver.html.twig', [
+            "articulo" => $articulo            
+        ]);
+    }
+
+
+    /**
+     * @Route("crear", name="crear")
+     */
+    public function new(Request $request,ManagerRegistry $doctrine): Response
+    {
+        
+        // creates a task object and initializes some data for this example
+        $articulo = new Articulo();
+
+        $form = $this->createForm(ArticuloType::class,$articulo);
+        $entityManager=$doctrine->getManager();
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $articulo = $form->getData();
+
+                $entityManager->persist($articulo);
+                $entityManager->flush();
+
+            return $this->redirectToRoute('/', [
+                'form' => $form]);
+            }
+        
+            return $this->renderForm('form.html.twig', ['form' => $form]);
+        
+    }
+
+
+    /**
+     * @Route("editar/{numero}", name="editar")
+     */
+    public function edit(Request $request,ManagerRegistry $doctrine,int $numero): Response
+    {
+        
+        $articuloEncontrado = $doctrine->getRepository(Articulo::class)->find($numero);
+        $entityManager =$doctrine->getManager();
+
+        if (!$articuloEncontrado) {
+            throw $this->createNotFoundException('No existe ningun articulo con el id:  ' . $numero);
+        } 
+        
+        $form = $this->createForm(ArticuloType::class , $articuloEncontrado);
+        $entityManager=$doctrine->getManager();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $articuloEcontrado=$form->getData();
+            $entityManager->persist($articuloEcontrado);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('/');
+        }    
+            return $this->renderform('editar.html.twig', ['form' => $form]);    
+    
+    }
+
+
+    /**
+     * @Route("eliminar/{numero}", name="eliminar")
+     */
+    public function eliminar(Request $request,ManagerRegistry $doctrine, int $numero) {
+
+  
+        $articuloBorrado = $doctrine->getRepository(Articulo::class)->find($numero);
+        $entityManager =$doctrine->getManager();
+
+        if (!$articuloBorrado) {
+            throw $this->createNotFoundException('No existe ningun articulo con este ID:  ' . $numero);
+        } 
+            $entityManager->remove($articuloBorrado);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('/');
+        }    
+    
+    }
